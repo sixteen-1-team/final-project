f32099b2cf3951b907c321ba56167e03560322a5
leejaeyun <zhcv321741185@gmail.comgit>
2025-08-22 17:41:33 +0900

    feat : play_random funcion

--- optional_task/prisoners_100_problem.py
+++ optional_task/prisoners_100_problem.py
@@ -1,8 +1,25 @@
 import random
 
 def play_random(n):
-    return random.randint(1, n)
-
+    # using 0-99 instead of ranges 1-100
+    pardoned = 0
+    in_drawer = list(range(100))
+    sampler = list(range(100))
+    for _round in range(n):
+        random.shuffle(in_drawer)
+        found = False
+        for prisoner in range(100):
+            found = False
+            for reveal in random.sample(sampler, 50):
+                card = in_drawer[reveal]
+                if card == prisoner:
+                    found = True
+                    break
+            if not found:
+                break
+        if found:
+            pardoned += 1
+    return pardoned / n * 100   # %
 
 def play_optimal(n):
     return random.randint(1, n)
