fcf847d9a25513db57015a852ca219941edac742
juny79 <quriquri7@gmail.com>
2025-08-22 17:31:28 +0900

    feat: print fizzbuzz

---
+++ fizzbuzz_juny79.py
@@ -0,0 +1,9 @@
+for j in range(1,30+1):
+    if j % 15 == 0:
+        print('fizzbuzz')
+    elif j % 3 == 0:
+        print('fizz')
+    elif j % 5 == 0:
+        print('buzz')
+    else:
+        print(j)
