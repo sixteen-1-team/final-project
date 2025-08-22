7685bbc8381cbfcd6d6d2fe95bb6fa47af17aaac
hyuk12 <hotman7025@naver.com>
2025-08-22 17:42:06 +0900

    feat: 선택과제 1번 play_optimal 함수 구현완료

--- optional_task/prisoners_100_problem.py
+++ optional_task/prisoners_100_problem.py
@@ -5,7 +5,25 @@ def play_random(n):
 
 
 def play_optimal(n):
-    return random.randint(1, n)
+    pardoned = 0
+    in_drawer = list(range(100))
+
+    for _round in range(n):
+        random.shuffle(in_drawer)
+        for prisoner in range(100):
+            reveal = prisoner
+            found = False
+            for go in range(50):
+                card = in_drawer[reveal]
+                if card == prisoner:
+                    found = True
+                    break
+                reveal = card
+            if not found:
+                break
+        if found:
+            pardoned += 1
+    return pardoned / n * 100   # %
 
 if __name__ == '__main__':
     n = 100_000
